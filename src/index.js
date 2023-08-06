7e3354bce17ea847dd621e77f3c5e58a035b64dd
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-08-06 16:09:00 -0600

    Add an app logic controller

--- src/index.js
+++ src/index.js
@@ -1,30 +1,66 @@
 import "./style.css";
-
-const taskFactory = (title, description, due_date, priority) => {
-    return {
-        title,
-        description,
-        due_date,
-        priority,
-        done: false,
-        created_at: new Date(),
-    }
-}
-
-const projectFactory = (name, task_list) => {
-    return {
-        name,
-        task_list,
-    }
-}
+import {Task, Project} from  "./modules/factories";
 
 const appController = (() => {
-    
+    let project_list = [];
+    let current_project;
+
+    const addProject = (project) => {
+        project_list.push(project);
+        changeCurrentProject(project);
+    };
+
+    const changeCurrentProject = (project) => current_project = project;
+
+    const deleteProject = (project) => {
+        project_list = project_list.filter(current_project => current_project != project);
+        current_project = project_list.length ? project_list[0] : null;
+    };
+
+    //const addTaskToCurrentProject = (task) => current_project.addTask(task);
+    //const deleteTaskFromCurrentProject = (task) => current_project.deleteTask(task);
+
+    const initialSetup = () => {
+        const my_project = Project("Today", []);
+        addProject(my_project);
+        changeCurrentProject(my_project);
+    };
+
+    const test = () => {
+        console.log(current_project.getName());
+        addProject(Project("Project Test", []));
+        const task1 = Task("Task1", "Desc1", new Date(), 1);
+        const task2 = Task("Task2", "Desc2", new Date(), 2);
+        console.log(current_project.getName());
+        console.log(current_project.getTaskList().map(task => task.getTitle()));
+        current_project.addTask(task1);
+        current_project.addTask(task2);
+        current_project.addTask(Task("Task3", "Desc3", new Date(), 1));
+        console.log(current_project.getTaskList()[2].getDone());
+        current_project.getTaskList()[2].toggleDone();
+        console.log(current_project.getTaskList()[2].getDone());
+        console.log(current_project.getTaskList().map(task => task.getTitle()));
+        current_project.deleteTask(task2);
+        current_project.addTask(Task("Task4", "Desc4", new Date(), 3));
+        console.log(current_project.getTaskList().map(task => task.getTitle()));
+        current_project.setName("Cool Project Test");
+        console.log(current_project.getName());
+        console.log(project_list.map(task => task.getName()));
+        deleteProject(current_project);
+        console.log(project_list.map(task => task.getName()));
+        console.log(current_project.getName());
+    };
+
+    return {
+        addProject,
+        changeCurrentProject,
+        deleteProject,
+        //addTaskToCurrentProject,
+        //deleteTaskFromCurrentProject,
+        initialSetup,
+        test,
+    }
 })();
 
-const myTask = taskFactory("Print book", "Print the book I need to print", new Date(), 1);
-console.log(myTask.title);
-console.log(myTask.title = "Cool new title");
-console.log(myTask.title);
-console.log(myTask.created_at);
-console.log(myTask);
+appController.initialSetup();
+appController.test();
---
+++ src/modules/factories.js
@@ -0,0 +1,63 @@
+const Task = (new_title, new_description, new_due_date, new_priority) => {
+    let title = new_title;
+    let description = new_description;
+    let due_date = new_due_date;
+    let priority = new_priority;
+    let done = false;
+    let created_at = new Date();
+
+    const getTitle = () => title;
+    const setTitle = (new_title) => title = new_title;
+
+    const getDescription = () => description;
+    const setDescription = (new_description) => description = new_description;
+
+    const getDueDate = () => due_date;
+    const setDueDate = (new_due_date) => due_date = new_due_date;
+
+    const getPriority = () => priority;
+    const setPriority = (new_priority) => priority = new_priority;
+
+    const getDone = () => done;
+    const toggleDone = () => done = done ? false : true;
+
+    const getCreatedAt = () => created_at;
+
+    return {
+        getTitle,
+        setTitle,
+        getDescription,
+        setDescription,
+        getDueDate,
+        setDueDate,
+        getPriority,
+        setPriority,
+        getDone,
+        toggleDone,
+        getCreatedAt
+    }
+}
+
+const Project = (initial_name, initial_task_list) => {
+    let name = initial_name;
+    let task_list = initial_task_list;
+
+    const getName = () => name;
+    const setName = (new_name) => name = new_name;
+    
+    const getTaskList = () => task_list;
+    const addTask = (task) => task_list.push(task);
+    const deleteTask = (task) => 
+        task_list = task_list.filter(current_task => current_task != task);
+
+
+    return {
+        getName,
+        setName,
+        getTaskList,
+        addTask,
+        deleteTask,
+    }
+}
+
+export {Task, Project};
