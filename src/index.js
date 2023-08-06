77c12000e7c1fa97a7a2ed6399b7fa83afd3472a
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-08-06 14:12:18 -0600

    Add task and project factories

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import "./style.css";
 
+const taskFactory = (title, description, due_date, priority) => {
+    return {
+        title,
+        description,
+        due_date,
+        priority,
+        done: false,
+        created_at: new Date(),
+    }
+}
+
+const projectFactory = (name, task_list) => {
+    return {
+        name,
+        task_list,
+    }
+}
+
+const appController = (() => {
+    
+})();
+
+const myTask = taskFactory("Print book", "Print the book I need to print", new Date(), 1);
+console.log(myTask.title);
+console.log(myTask.title = "Cool new title");
+console.log(myTask.title);
+console.log(myTask.created_at);
+console.log(myTask);
