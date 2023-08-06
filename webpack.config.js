50af37ccb2fd646bbdd54701c971c5f5653fd924
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-08-05 19:44:51 -0600

    Setup repository structure

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    mode: "development",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Todo List',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+};
